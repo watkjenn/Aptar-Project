7baa8c2948457728d1b81a45af28ed4877d3c534
watkjenn <watkins.jenna.99@gmail.com>
2016-04-28 14:36:22 -0500

    bh

--- Final Projectish/BlackberryHelper.aspx.designer.cs
+++ Final Projectish/BlackberryHelper.aspx.designer.cs
@@ -22,6 +22,33 @@ namespace Final_Projectish {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// fn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText fn;
+        
+        /// <summary>
+        /// ln control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText ln;
+        
+        /// <summary>
+        /// pp control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText pp;
+        
+        /// <summary>
         /// SubmitButton control.
         /// </summary>
         /// <remarks>
